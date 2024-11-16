8b59b681391ad9ee17d7907ed9fafee363b09ad7
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-11-16 20:58:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function isSameType(value1, value2) {
         return true; 
     }
 	
-    return typeof value1 === typeof value2;
-
+   if(typeof value1 === typeof value2)
+   {
+	   return true;
+   }
+	return false;
+	
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
